b29ff4fb0d97d52340f0ee25f1872a6304edfdda
hridoy <ariyanhridoy921@gmail.com>
2026-02-18 19:00:02 +0600

    Product Class Created

--- Sales_analytics.py
+++ Sales_analytics.py
@@ -12,4 +12,25 @@ class Product:
     
     def __str__(self):
         return f' {self.name} | Category : {self.category} | Price : {self.price}'
+
+class Sale:
+    
+    def __init__(self , sale_id , product ,quantity , date_str):
+        
+     self.sale_id = sale_id
+     self.product=product
+     self.quantity = quantity
+     
+     self.date = datetime.strptime(date_str ,"%d-%m-%Y" )
+     
+    def get_revenue(self):
+        
+        return self.quantity*self.product.price
+    
+    def __str__(self):
+        return ( f'Sale {self.sale_id} -> {self.product}'
+                 f" x{self.quantity} Units"
+                 f' = {self.get_revenue} TK'
+                 f" On {self.date.strftime('%B %d %Y')}"
+                )
     
